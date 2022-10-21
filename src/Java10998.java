0a9da92c9ebd76b84ef041fba74b3f76e9f0f43a
wusjustdoit <wusjustdoit@gmail.com>
2022-10-22 05:34:27 +0900

    1st commit

---
+++ src/Java10998.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Java10998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+        System.out.println(i * j);
+    }
+}
---
+++ src/Java2557.java
@@ -0,0 +1,5 @@
+public class Java2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
