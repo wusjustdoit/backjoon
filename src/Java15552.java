3cc5c7c45fb7d4f8db619d38207e964d42a4855d
wusjustdoit <wusjustdoit@gmail.com>
2022-10-30 19:20:44 +0900

    20221027 backjoon

--- src/Java15552.java
+++ src/Java15552.java
@@ -31,5 +31,6 @@ public class Java15552 {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer();
+
     }
 }
