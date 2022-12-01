6c76587a1811e151140dc87100e21be7f69703eb
wusjustdoit <wusjustdoit@gmail.com>
2022-12-01 11:43:12 +0900

    20221201 backjoon

---
+++ src/Java2438.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/*
+문제
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+ */
+public class Java2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+
+        int starNum = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=starNum; i++) {
+            for(int j=1; j<=i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ src/Java2439.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/*
+문제
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+ */
+public class Java2439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=n; i++) {
+            for(int j=(n-i); j>= 1; j--) {
+                bw.write(" ");
+            }
+            for(int j=1; j<=i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
