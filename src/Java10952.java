9ceb3c373e95a8511ffd82c8e05c0c57ced2baa4
wusjustdoit <wusjustdoit@gmail.com>
2022-12-01 12:24:27 +0900

    20221201 backjoon

---
+++ src/Java10952.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/*
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+입력은 여러 개의 테스트 케이스로 이루어져 있다.
+
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+입력의 마지막에는 0 두 개가 들어온다.
+
+출력
+각 테스트 케이스마다 A+B를 출력한다.
+ */
+public class Java10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            st = new StringTokenizer(br.readLine()," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a==0 && b==0) {
+                break;
+            }
+            sb.append(a+b).append('\n');
+        }
+        System.out.println(sb);
+
+
+    }
+}
